991b1b961e2f0a5c087ff48fc255c82ac73d90ee
xj63 <xj63work@163.com>
2025-04-07 23:56:14 +0800

    feat: Add geo properties to JSON response
    Add:
    - country
    - countryFlag
    - region
    - timezone
    - latitude
    - longitude
    - clientTcpRtt
    - ip

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,19 @@ function getHtml(): Response {
 }
 
 function getGeo(request: Request): Response {
-	const data = {};
+	const country = request.cf?.country as string | null;
+	const countryFlag = country ? countryCodeToFlagEmoji(country) : null;
+	const data = {
+		country,
+		countryFlag,
+		region: request.cf?.region,
+		timezone: request.cf?.timezone,
+		latitude: request.cf?.latitude,
+		longitude: request.cf?.longitude,
+		clientTcpRtt: request.cf?.clientTcpRtt,
+		ip: request.headers.get('cf-connecting-ip'),
+	};
+
 	return Response.json(data, {
 		headers: {
 			...DEFAULT_HEADERS,
@@ -24,6 +36,7 @@ function getGeo(request: Request): Response {
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+		console.log(request);
 		const acceptHeader = request.headers.get('Accept');
 
 		// Accept html
