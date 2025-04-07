8542362a17bf0adadd9896d05fdfc19acf20798b
xj63 <xj63work@163.com>
2025-04-07 22:57:37 +0800

    feat: Serve HTML page on HTML requests

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+// @ts-ignore
+import homeHtml from './index.html';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+		const acceptHeader = request.headers.get('Accept');
+
+		// Accept html
+		if (acceptHeader?.includes('text/html')) {
+			return new Response(homeHtml, {
+				headers: {
+					'Content-Type': 'text/html',
+				},
+			});
+		}
+
 		return new Response('Hello World!');
 	},
 } satisfies ExportedHandler<Env>;
